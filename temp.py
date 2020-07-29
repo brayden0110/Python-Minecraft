0526073229b71b51dc0d39e7140af7989b769122
brayden0110 <68846786+brayden0110@users.noreply.github.com>
2020-07-29 16:38:46 +0800

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+x,y,z = mc.player.getTilePos()
+while True:
+    hits = mc.events.pollProjectileHits()
+    if len(hits)>0:
+        hit = hits[0]
+        x,y,z = hit.pos.x, hit.pos.y ,hit.pos.z
+        mc.createExplosion(x,y,z)
+
